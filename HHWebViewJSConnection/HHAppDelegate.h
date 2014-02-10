d4cffe5bc78176b5349d12e12f4cdd8ef696e64c
hanjize <hanjize@rd.tuan800.com>
2014-02-10 15:29:14 +0800

    iOS & JS Connection
    init iOS & JS Connection

---
+++ HHWebViewJSConnection/HHAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  HHAppDelegate.h
+//  HHWebViewJSConnection
+//
+//  Created by LeAustinHan on 14-2-10.
+//  Copyright (c) 2014年 hanhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class HHViewController;
+
+@interface HHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) HHViewController *viewController;
+
+@end
---
+++ HHWebViewJSConnection/HHViewController.h
@@ -0,0 +1,13 @@
+//
+//  HHViewController.h
+//  HHWebViewJSConnection
+//
+//  Created by LeAustinHan on 14-2-10.
+//  Copyright (c) 2014年 hanhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HHViewController : UIViewController
+
+@end
